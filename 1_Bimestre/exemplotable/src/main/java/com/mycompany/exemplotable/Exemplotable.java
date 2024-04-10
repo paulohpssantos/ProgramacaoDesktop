4aa304f07a1ae4472208b6d26b0baf6871996a03
Paulo Henrique Pereira dos Santos <84387138+paulohpssantos@users.noreply.github.com>
2024-04-10 19:52:24 -0300

    Material
    Exemplocom tabelas

---
+++ 1_Bimestre/exemplotable/src/main/java/com/mycompany/exemplotable/Exemplotable.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exemplotable;
+
+/**
+ *
+ * @author paulodossantos
+ */
+public class Exemplotable {
+
+    public static void main(String[] args) {
+        new PrincipalFrame().setVisible(true);
+    }
+}
